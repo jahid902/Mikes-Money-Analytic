be7c1cad74f53f974aebda6aa8b472f1ca7133ec
jahid902 <jahidjr902@gmail.com>
2023-07-22 21:11:52 +0600

    completed whole website design, dekstop design compette

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { PiButterflyDuotone, PiNotificationLight } from "react-icons/Pi";
 import { BiSearchAlt, BiHomeAlt, BiLogOut } from "react-icons/Bi";
-import { FaRegNewspaper, FaNewspaper, FaAngleDown } from "react-icons/Fa";
+import {
+  FaRegNewspaper,
+  FaNewspaper,
+  FaAngleDown,
+  FaAngleRight,
+} from "react-icons/Fa";
 import avatar from "/profile.png";
 import MoneyChart from "./MoneyChart";
-import LoadingSpinner from "./LoadingSpinner";
-import LoadingSpinner2 from "./LoadingSpinner2";
-import LoadingSpinner3 from "./LoadingSpinner3";
-
 
 function App() {
   return (
     <>
       <div className="font-serif flex flex-col md:flex-row gap-2 bg-gradient-to-b from-slate-200 to-slate-400 px-2 py-4">
         {/* nav section */}
-        <div className="w-[5%] rounded-md items-center flex flex-row md:flex-col justify-between py-6 bg-white px-3">
+        <div className="w-[5%] rounded-md items-center flex flex-row md:flex-col justify-between py-6 bg-gradient-to-t from-slate-200 to-slate-100 px-3">
           <div className="space-y-4">
             <PiButterflyDuotone
               size={25}
@@ -37,7 +38,7 @@ function App() {
         </div>
 
         {/* profile section */}
-        <div className="w-[25%] rounded-md space-y-8 flex flex-col bg-gradient-to-t from-slate-100 via-slate-200 to-slate-300 px-6 py-10">
+        <div className="w-[25%] rounded-md space-y-8 flex flex-col bg-gradient-to-t from-slate-50 via-slate-200 to-slate-300 px-6 py-10">
           <div className="flex justify-start gap-3">
             <img
               className="w-14 h-14 rounded-full"
@@ -93,7 +94,7 @@ function App() {
         </div>
 
         {/* graph section */}
-        <div className="w-[45%] bg-gradient-to-t from-slate-100 to-slate-200 space-y-4 flex flex-col rounded-md bg-white py-10 px-8">
+        <div className="w-[45%] bg-gradient-to-t from-slate-200 to-slate-100 space-y-4 flex flex-col rounded-md bg-white py-10 px-8">
           <div>
             <h5 className="text-sky-700 font-semibold text-sm">
               Retirement Income
@@ -111,43 +112,145 @@ function App() {
             </div>
             <div className="py-3 w-full border-b-2 border-sky-700">
               <h1 className="font-bold text-2xl">$300</h1>
-              <h6 className="text-xs mt-1 text-slate-500">Est. Monthly Income.</h6>
+              <h6 className="text-xs mt-1 text-slate-500">
+                Est. Monthly Income.
+              </h6>
             </div>
           </div>
           <div>
             <h3 className="font-bold ">Contributions OverTime</h3>
             <div className="flex gap-2 my-3 justify-between">
-              <span className="bg-sky-800 h-[10px] rounded-full flex my-auto text-slate-200">...</span>
-              <p className="font-sans text-xs">Employer: <span className="font-bold text-xs">K 73,500</span></p>
-              <span className="bg-sky-600 h-[10px] rounded-full flex my-auto text-slate-200">...</span>
-              <p className="font-sans text-xs">Employee: <span className="font-bold text-xs">K 52,500</span></p>
-              <span className="bg-sky-400 h-[10px] rounded-full flex my-auto text-slate-200">...</span>
-              <p className="font-sans text-xs">Total Interest: <span className="font-bold text-xs">K 244,313</span></p>
+              <span className="bg-violet-600 h-[10px] rounded-full flex my-auto text-slate-200">
+                ...
+              </span>
+              <p className="font-sans text-xs">
+                Employer: <span className="font-bold text-xs">K 73,500</span>
+              </p>
+              <span className="bg-teal-500 h-[10px] rounded-full flex my-auto text-slate-200">
+                ...
+              </span>
+              <p className="font-sans text-xs">
+                Employee: <span className="font-bold text-xs">K 52,500</span>
+              </p>
+              <span className="bg-black h-[10px] rounded-full flex my-auto text-slate-200">
+                ...
+              </span>
+              <p className="font-sans text-xs">
+                Total Interest:{" "}
+                <span className="font-bold text-xs">K 244,313</span>
+              </p>
             </div>
             <MoneyChart></MoneyChart>
           </div>
           <div>
-          <h1 className="font-bold text-lg">
+            <h1 className="font-bold text-lg">
               How Do I Compare My peers? <br />{" "}
-              <span className="font-normal text-sm">These Numbers represents current goal achievements.</span>
+              <span className="font-normal text-sm">
+                These Numbers represents current goal achievements.
+              </span>
             </h1>
           </div>
           <div className="flex gap-12 ">
             <div className="space-y-2">
-            <p className="border-b-2  border-stone-400 pb-1 flex gap-2 text-xs">Age: <span className="font-bold text-xs flex ">Under 30 <FaAngleDown className="flex my-auto"></FaAngleDown></span></p>
-            <p className="border-b-2 border-stone-400 pb-1 flex gap-2 text-xs">Salary: <span className="font-bold text-xs flex ">K 20 - K 30<FaAngleDown className="flex my-auto"></FaAngleDown></span></p>
-            <p className="border-b-2 border-stone-400 pb-1 flex gap-2 text-xs">Gender: <span className="font-bold text-xs flex ">Male <FaAngleDown className="flex my-auto"></FaAngleDown></span></p>
+              <p className="border-b-2  border-stone-400 pb-1 flex gap-2 text-xs">
+                Age:{" "}
+                <span className="font-bold text-xs flex ">
+                  Under 30 <FaAngleDown className="flex my-auto"></FaAngleDown>
+                </span>
+              </p>
+              <p className="border-b-2 border-stone-400 pb-1 flex gap-2 text-xs">
+                Salary:{" "}
+                <span className="font-bold text-xs flex ">
+                  K 20 - K 30
+                  <FaAngleDown className="flex my-auto"></FaAngleDown>
+                </span>
+              </p>
+              <p className="border-b-2 border-stone-400 pb-1 flex gap-2 text-xs">
+                Gender:{" "}
+                <span className="font-bold text-xs flex ">
+                  Male <FaAngleDown className="flex my-auto"></FaAngleDown>
+                </span>
+              </p>
             </div>
             <div className="flex gap-5">
-              <LoadingSpinner2></LoadingSpinner2>
-              <LoadingSpinner></LoadingSpinner>
-              <LoadingSpinner3></LoadingSpinner3>
+              <div
+                className="radial-progress text-primary"
+                style={{ "--value": 90 }}
+              >
+                90%
+              </div>
+              <div
+                className="radial-progress text-primary"
+                style={{ "--value": 75 }}
+              >
+                75%
+              </div>
+              <div
+                className="radial-progress text-primary"
+                style={{ "--value": 65 }}
+              >
+                65%
+              </div>
             </div>
           </div>
         </div>
 
         {/* calc section */}
-        <div className="w-[25%]"></div>
+        <div className="w-[25%] space-y-6  rounded-md py-10 px-8 flex flex-col bg-gradient-to-t from-slate-50  via-slate-200 to-slate-300">
+          <div>
+            <h2 className="font-bold text-lg">Retirement Strategy</h2>
+          </div>
+          <div>
+            <h1 className="font-medium pb-3">Employee Contribution</h1>
+            <div className="flex gap-4">
+              <input
+                type="range"
+                min="0"
+                max="100"
+                className="range range-xs my-auto range-primary"
+              />
+              <span className="font-bold bg-slate-200 p-2 rounded-md">12%</span>
+            </div>
+          </div>
+          <div>
+            <h1 className="font-medium pb-3">Retirement Age</h1>
+            <div className="flex gap-4">
+              <input
+                type="range"
+                min="0"
+                max="100"
+                className="range range-xs my-auto range-primary"
+              />
+              <span className="font-bold bg-slate-200 p-2 rounded-md">65</span>
+            </div>
+          </div>
+          <hr className="h-0.5 bg-slate-400" />
+          <div className="space-y-8">
+            <div className="flex justify-between">
+              <h4 className="font-bold text-sm">Employer Contribution</h4>
+              <p className="font-bold text-sm">8.4%</p>
+            </div>
+            <div className="flex justify-between">
+              <h4 className="font-bold text-sm">Interest Rate</h4>
+              <p className="font-bold text-sm">5%</p>
+            </div>
+            <div className="space-y-4">
+              <button className="btn btn-primary w-full">Update</button>
+              <p className="flex gap-1 justify-center text-blue-800">
+                View help docs{" "}
+                <FaAngleRight className="flex my-auto"></FaAngleRight>
+              </p>
+            </div>
+            <div className="flex flex-col justify-start pl-1 space-y-2 border-l-2 border-sky-800">
+              <h1 className="text-slate-600 ">Are you considering a <br /> <span className="font-bold">Housing Advance?</span></h1>
+              <p className="text-xs text-slate-500">Limited time reduced interest</p>
+              <p className="flex text-sm justify-start text-blue-800">
+               Learn more
+                <FaAngleRight className="flex my-auto"></FaAngleRight>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
